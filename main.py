428f6a1faf5b8903625ba10dabb91c967bf45a01
Nathan <lassinganathan@gmail.com>
2025-02-06 22:45:56 -0800

    fix gitignore

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from SessionController import SessionController
 
 def main(): 
     # Create a list from the following string
-    listString = "1 package whole wheat, 2 cups milk, 6 lbs ground beef, 4 cups cheddar cheese, 1 package pizza, 2 oz broccoli, 2 lbs ground beef"
+    listString = "1 package whole wheat, 2 cups milk, 6 lbs ground beef, 4 cups cheddar cheese, 1 package pizza, 2 oz broccoli, 2 lbs ground beef, 1 cup yogurt, 2 cuts steak"
     
     sessionController = SessionController()
     
-    print(sessionController.createList(listString))
-    
-    sessionController.renameCategory(0, "milks")
+    sessionController.addFoodItemToCategory(1, "steak")
     
     print(sessionController.createList(listString))
+    
         
 main()
